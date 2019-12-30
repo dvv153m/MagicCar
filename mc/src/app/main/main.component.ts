420be5fcba6d8ef4e36a07c6edb25676eaf63379
dvv153m <biker-moto@mail.ru>
2019-12-31 03:41:03 +0400

    fixs

--- mc/src/app/main/main.component.ts
+++ mc/src/app/main/main.component.ts
@@ -35,8 +35,10 @@ export class MainComponent{
 
     hexToBytes(hex) : number[] {
 
-        for (var bytes = [], c = 0; c < hex.length; c += 2)
-        bytes.push(parseInt(hex.substr(c, 2), 16));
+        for (var bytes = [], c = 0; c < hex.length; c += 2){
+         
+            bytes.push(parseInt(hex.substr(c, 2), 16));
+        }
         return bytes;
     }
 
